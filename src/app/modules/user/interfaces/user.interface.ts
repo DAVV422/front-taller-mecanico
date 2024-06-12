35edd11f86d383579d693730e106611c5c9e0de9
DAVV422 <diegoalberto42216@gmail.com>
2024-06-12 01:05:01 -0400

    feat(personal): create and list personal

--- src/app/modules/user/interfaces/user.interface.ts
+++ src/app/modules/user/interfaces/user.interface.ts
@@ -1,6 +1,16 @@
 export interface User {
-    id: number,
+    id: string,
     email: string,
     nombreUsuario: string,
     tipo: string,    
   }
+
+export interface Personal {
+  id: string,
+  nombre: string,
+  apellido: string,
+  celular: string,
+  ci: string,
+  direccion: string,
+  fechaNacimiento: string
+}
--- src/app/modules/user/pages/new-personal/new-personal.component.ts
+++ src/app/modules/user/pages/new-personal/new-personal.component.ts
@@ -1,12 +1,97 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Apollo, gql } from 'apollo-angular';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
+const createPersonal = gql`
+      mutation createPersonal(
+        $nombre:String!, 
+        $apellido:String!, 
+        $celular:String!,
+        $direccion:String!,
+        $ci:String!,
+        $fechaNacimiento:String!,  	
+      ) {
+      createPersonal(
+        nombre:$nombre,
+        apellido:$apellido,
+        celular:$celular,
+        direccion:$direccion,
+        ci:$ci,
+        fechaNacimiento:$fechaNacimiento    
+      ){
+        id,
+        nombre,
+        apellido,
+        ci,
+        direccion,
+        celular,
+        fechaNacimiento,
+        usuarioId
+      }
+    }
+  `;
 @Component({
   selector: 'app-new-personal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, NgClass, NgIf, AngularSvgIconModule, ButtonComponent],
   templateUrl: './new-personal.component.html',
   styleUrl: './new-personal.component.scss'
 })
 export class NewPersonalComponent {
+  form!: FormGroup;
+  submitted = false;
+  passwordTextType!: boolean;
+  disabled: boolean = false;
 
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+    private readonly router: Router,
+    private readonly apollo: Apollo
+  ) {}
+
+  onClick() {
+    console.log('Button clicked');
+  }
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      ci: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      celular: ['', [Validators.required]],
+      fechaNacimiento: ['', [Validators.required]],      
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.disabled = true;
+    const { nombre, apellido, ci, direccion, celular, fechaNacimiento } = this.form.value;
+    this.apollo.mutate({ 
+      mutation: createPersonal,
+      variables: { nombre, apellido, ci, direccion, celular, fechaNacimiento}
+    }).subscribe(
+      ({data}) => {
+        if(data) {
+          this.router.navigate(['/taller/users/personal']);
+        }        
+      }
+    );
+  }
+
+  cancelar(){
+    this.router.navigate(['/taller/users/personal']);
+  }
 }

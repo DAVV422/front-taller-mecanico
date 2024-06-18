221eb642ddfae3a28483f63d83dcc142b36e939e
DAVV422 <diegoalberto42216@gmail.com>
2024-06-17 21:05:41 -0400

    fix(NotaVenta): fix reload

--- src/app/modules/ventas/components/detalle-service-table/detalle-service-table.component.ts
+++ src/app/modules/ventas/components/detalle-service-table/detalle-service-table.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { DetalleVentaServicio } from '../../interfaces/venta.interface';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { DetalleVentaServicio, NotaVenta } from '../../interfaces/venta.interface';
 import { DetalleServiceTableItemComponent } from '../detalle-service-table-item/detalle-service-table-item.component';
 import { NgFor } from '@angular/common';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { GET_ALL_DETALLE_VENTA_SERVICIO_OF_NOTA_VENTA } from 'src/app/core/constants/query';
 
@@ -13,16 +13,25 @@ import { GET_ALL_DETALLE_VENTA_SERVICIO_OF_NOTA_VENTA } from 'src/app/core/const
   templateUrl: './detalle-service-table.component.html',
   styleUrl: './detalle-service-table.component.scss'
 })
-export class DetalleServiceTableComponent {  
+export class DetalleServiceTableComponent implements OnInit, OnDestroy {  
   public activeAuction2: DetalleVentaServicio[] = [];
+  public id!: String;
+  public query!: any;
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private readonly apollo: Apollo
-  ) {}
+  ) {
+    this.route.params.subscribe(
+      (params: Params) => this.id = params['id']
+    );
+  }
 
-  ngOnInit(): void {
-    const notaVentaId: String = "666f67e84eecc74417220127";
+  ngOnInit(): void {    
+    console.log("inicia el componente");
+    const notaVentaId: String = this.id;
+    console.log(notaVentaId);
     this.apollo
       .watchQuery({
         query: GET_ALL_DETALLE_VENTA_SERVICIO_OF_NOTA_VENTA,
@@ -35,10 +44,11 @@ export class DetalleServiceTableComponent {
   }
 
   newDetalleServicio(): void {
-    this.router.navigate(['/taller/ventas/detalleServicio/new']);
+    const id = this.id
+    this.router.navigate(['/taller/ventas/detalleServicio/new', id]);
   }
 
-  ngOnDestroy(){
-
+  ngOnDestroy(): void {
+    this.query.unSubscribe()
   }
 }
--- src/app/modules/ventas/components/detalle-table/detalle-table.component.ts
+++ src/app/modules/ventas/components/detalle-table/detalle-table.component.ts
@@ -1,10 +1,10 @@
 import { NgFor } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { DetalleTableItemComponent } from '../detalle-table-item/detalle-table-item.component';
-import { DetalleVentaProducto, DetalleVentaServicio } from '../../interfaces/venta.interface';
-import { Router } from '@angular/router';
-import { Apollo } from 'apollo-angular';
-import { GET_ALL_DETALLE_VENTA_PRODUCTO_OF_NOTA_VENTA, GET_ALL_DETALLE_VENTA_SERVICIO_OF_NOTA_VENTA } from 'src/app/core/constants/query';
+import { DetalleVentaProducto, DetalleVentaServicio, NotaVenta } from '../../interfaces/venta.interface';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Apollo, Subscription } from 'apollo-angular';
+import { GET_ALL_DETALLE_VENTA_PRODUCTO_OF_NOTA_VENTA } from 'src/app/core/constants/query';
 
 @Component({
   selector: '[detalle-table]',
@@ -15,16 +15,24 @@ import { GET_ALL_DETALLE_VENTA_PRODUCTO_OF_NOTA_VENTA, GET_ALL_DETALLE_VENTA_SER
 })
 export class DetalleTableComponent implements OnInit, OnDestroy {
   public activeAuction: DetalleVentaProducto[] = [];
-  public activeAuction2: DetalleVentaServicio[] = [];
+  public id!: String;
+  public query!: any;
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private readonly apollo: Apollo
-  ) {}
+  ) {
+    this.route.params.subscribe(
+      (params: Params) => this.id = params['id']
+    );
+  }
 
   ngOnInit(): void {
-    const notaVentaId: String = "666f67e84eecc74417220127";       
-    this.apollo
+    
+    const notaVentaId: String = this.id;
+    console.log(notaVentaId)
+    this.query = this.apollo
       .watchQuery({
         query: GET_ALL_DETALLE_VENTA_PRODUCTO_OF_NOTA_VENTA,
         variables: { notaVentaId }
@@ -32,23 +40,15 @@ export class DetalleTableComponent implements OnInit, OnDestroy {
         if(result.data.getAllDetalleVentaProductoOfNotaVenta != null){
           this.activeAuction = result.data.getAllDetalleVentaProductoOfNotaVenta;          
         }
-      }); 
-      this.apollo
-      .watchQuery({
-        query: GET_ALL_DETALLE_VENTA_SERVICIO_OF_NOTA_VENTA,
-        variables: { notaVentaId }
-      }).valueChanges.subscribe(( result: any) => {
-        if(result.data.getAllDetalleVentaServicioOfNotaVenta != null){
-          this.activeAuction2 = result.data.getAllDetalleVentaServicioOfNotaVenta;          
-        }
-      }); 
+      });
   }
 
   newDetalleProducto(): void {
-    this.router.navigate(['/taller/ventas/detalle/new']);
+    const id: String = this.id;
+    this.router.navigate(['/taller/ventas/detalleProducto/new', id]);
   }
 
-  ngOnDestroy(){
-
+  ngOnDestroy(): void {
+    this.query.unSubscribe()
   }
 }

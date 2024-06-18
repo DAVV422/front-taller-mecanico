e519c52bf56181137fc28ffe2e3a1b004d76df63
DAVV422 <diegoalberto42216@gmail.com>
2024-06-17 21:29:58 -0400

    feat(NotaVenta): eliminar detalle

--- src/app/modules/ventas/components/detalle-service-table-item/detalle-service-table-item.component.ts
+++ src/app/modules/ventas/components/detalle-service-table-item/detalle-service-table-item.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { DetalleVentaServicio } from '../../interfaces/venta.interface';
 import { CurrencyPipe } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Apollo } from 'apollo-angular';
+import { DELETE_DETALLE_VENTA_SERVICIO } from 'src/app/core/constants/mutation';
 
 @Component({
   selector: '[detalle-service-table-item]',
@@ -12,4 +14,22 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 })
 export class DetalleServiceTableItemComponent {
   @Input() auctionServicio = <DetalleVentaServicio>{};  
+
+  constructor(
+    private readonly apollo: Apollo
+  ){}
+  eliminar(id: String){
+    this.apollo.mutate({ 
+      mutation: DELETE_DETALLE_VENTA_SERVICIO,
+      variables: { id }
+    }).subscribe(
+      ({data}) => {
+        console.log(id);
+        console.log(data);
+        if( data ) {
+          console.log("Detalle Eliminado");
+        }        
+      }
+    );    
+  }
 }
